61e04f9977f24c35381d1810d57ff0d699325d97
Schraibikus <kurt-77@mail.ru>
2025-02-23 12:20:18 +0500

    для размещения на сервере поменял пути в axios.ts

--- src/store/axios.ts
+++ src/store/axios.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://45.9.75.34:5000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+
 
